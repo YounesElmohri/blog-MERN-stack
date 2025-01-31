e2983d0c0e3861bd8115d1d2209ab591dafb7ffa
YounesElmohri <91877394+YounesElmohri@users.noreply.github.com>
2025-01-31 18:09:09 +0100

    Update request.js

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-    baseURL: "http://localhost:8000"
+    baseURL: "https://blog-mern-stack-backend.onrender.com"
 });
 
 export default request;
